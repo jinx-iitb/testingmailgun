6dd568a38682b37153271685ecb0f20d8c29674b
Ajinkya <203050032@iitb.ac.in>
2020-11-04 16:50:02 +0530

    first commit - test

---
+++ src/main/java/tetsing/test1.java
@@ -0,0 +1,36 @@
+package tetsing;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class test1 {
+
+    public static void main(String[] args) throws IOException {
+        String key = "2e4e8ef329f88db6b884ddaa49c7419e-ea44b6dc-5d92ceb3";
+        String url = "https://api.mailgun.net/v3/" + "sandbox286a43229b2645c7ba17e5bd0bab86b2.mailgun.org" + "/messages";
+                //https://api.mailgun.net/v3/sandbox286a43229b2645c7ba17e5bd0bab86b2.mailgun.org";
+        String from = "yolo@iitb.ac.in";
+        String[] emails = new String[]{"jumbadajinkya@gmail.com","203050032@iitb.ac.in"};
+        String fullEmailString = String.join(",", emails);
+        OkHttpClient client = new OkHttpClient();
+        RequestBody formBody = new FormBody.Builder()
+                .add("from", from)
+                .add("to", fullEmailString)
+                .add("subject", "mailgun test 4")
+                .add("text", "ignore this")
+                .build();
+        String credential = Credentials.basic("api", key);
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Authorization", credential)
+                .post(formBody)
+                .build();
+        Call call = client.newCall(request);
+        Response response = call.execute();
+        System.out.println(response.body().string());
+
+    }
+
+
+}
